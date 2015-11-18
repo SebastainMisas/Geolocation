5597f8605fa1a00e18065092aecf06c51cd80477
Sebastain Misas <juanmisas2@gmail.com>
2015-11-18 16:48:34 -0500

    Added google maps funtionality and autocompleate api

--- public/geolocation.js
+++ public/geolocation.js
@@ -1,24 +1,14 @@
 var map;
 
+function onError(err){
+  console.log("What are you using, IE 7??", err);
+}
+
+
 if ("geolocation" in navigator){
   navigator.geolocation.getCurrentPosition(onLocation, onError);
 }
 
-function onLocation(position){
-  // We can't just directly feed the position into our google map
-  // The objects are formatted differently, google maps is looking for
-  // an object with "lat" and "lng" keys.
-  var myPosition = {
-    lat: position.coords.latitude,
-    lng: position.coords.longitude
-  };
-
-  createMap(myPosition);
-}
-
-function onError(err){
-  console.log("What are you using, IE 7??", err);
-}
 
 function createMap(position){
   map = new google.maps.Map($('#map')[0], {
@@ -26,3 +16,44 @@ function createMap(position){
     zoom: 17
   });
 }
+
+function createMarker(position) {
+  var marker = new google.maps.Marker({
+   position: position,
+   map: map
+ });
+}
+
+
+
+
+
+
+
+function onLocation(position) {
+ 
+  var myPosition = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude,
+  };
+
+  createMap(myPosition);
+  createMarker(myPosition);
+  setupAutocomplete();
+}
+
+
+function setupAutocomplete(){
+  var input = $('#get-places')[0];
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.addListener('place_changed', function(){
+    var place = autocomplete.getPlace();
+    if (place.geometry.location) {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    } else {
+      alert("The place has no location...?")
+    }
+  });
+}
+
